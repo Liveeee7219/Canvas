0288fbe34721a561fb349c7bb9c59b56cda5a576
Levi <dlevijackson2@gmail.com>
2025-01-05 00:48:52 -0500

    Initial commit

---
+++ JS/panic.js
@@ -0,0 +1,9 @@
+window.addEventListener("keydown",
+    checkKeyPressed, false);
+
+function checkKeyPressed(evt) {
+    if (evt.keyCode == "27") {
+        window.location.replace(
+            "google.com");
+    }
+}
